cc8063daea36484f4bbe273ea664fcaf27dfd86e
Imre Kovács <2daymc@gmail.com>
2021-03-02 16:48:21 +0100

    Wrong placement, Deleted

--- src/main/GeoLocation.java
+++
@@ -1,6 +0,0 @@
-package geolocator;
-
-/**
- * Class for wrapping geolocation information about IP address.
- */
-record GeoLocation(String as, String city, String country, String countryCode, String isp, double lat, double lon, String org, String query, String region, String regionName, String timezone, String zip) {}
